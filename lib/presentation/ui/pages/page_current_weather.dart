efc3b80fc4988122a70c8985b5ea228a8404a2f8
dimaszisby <dimaszisby@gmail.com>
2022-08-08 13:22:44 +0700

    Add: Dependencies for slide-up panel

--- lib/presentation/ui/pages/page_current_weather.dart
+++ lib/presentation/ui/pages/page_current_weather.dart
@@ -53,7 +53,9 @@ class _CurrentWeatherPageState extends State<CurrentWeatherPage> {
                     crossAxisAlignment: CrossAxisAlignment.center,
                     key: const Key('weather_Data'),
                     children: [
-                      const HeaderSegment(country: 'Indonesia'), //TODO: Replaced with GeoLocator and GeoCoding
+                      const HeaderSegment(
+                          country:
+                              'Indonesia'), //TODO: Replaced with GeoLocator and GeoCoding
                       CoordinateCard(
                         lon: '${state.result.lon}',
                         lat: '${state.result.lat}',
