52643aac47cb7798ec2dffcd1e00ce11f6d7d096
dimaszisby <dimaszisby@gmail.com>
2022-08-09 11:33:02 +0700

    Update: update new UI for /card_forecast_Item.dart

--- lib/presentation/ui/molecules/card_forecast_Item.dart
+++ lib/presentation/ui/molecules/card_forecast_Item.dart
@@ -1,16 +1,51 @@
 import 'package:flutter/material.dart';
 
+import '../../../data/constants.dart';
+
 class ForecastItemCard extends StatelessWidget {
-  const ForecastItemCard({Key? key}) : super(key: key);
+  const ForecastItemCard({
+    Key? key,
+    required this.dateTime,
+    required this.main,
+    required this.icon,
+    required this.temp,
+    required this.winSpeed,
+  }) : super(key: key);
+
+  final String dateTime;
+  final String main;
+  final String icon;
+  final String temp;
+  final String winSpeed;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.white,
+      height: 20,
+      width: MediaQuery.of(context).size.width * 0.8,
       decoration: const BoxDecoration(
         borderRadius: BorderRadius.all(Radius.circular(10)),
         color: Colors.white,
       ),
+      child: Row(children: [
+        Text(dateTime),
+        Row(
+          children: [
+            // Image(
+            //   image: NetworkImage(
+            //     Urls.weatherIcon(icon),
+            //   ),
+            // ),
+            Text(main),
+          ],
+        ),
+        Row(
+          children: [
+            Text(temp),
+            Text(winSpeed),
+          ],
+        )
+      ]),
     );
   }
 }
