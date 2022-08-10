a8c83faf4ea5750e1ff3a1ba248e9844bee3bcff
dimaszisby <dimaszisby@gmail.com>
2022-08-10 14:29:58 +0700

    Add: class for /get_current_location.dart use case

---
+++ lib/domain/usecases/get_current_location.dart
@@ -0,0 +1,59 @@
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
+
+class GetLocation {
+
+  String location = 'Null, Press Button';
+  String address = 'search';
+  late String country = 'Null';
+
+  double lon = 0;
+  double lat = 0; 
+
+  double get newlon => lon;
+  double get newlat => lat;
+
+  late String finalCountry;
+
+
+  Future<Position> getGeoLocationPosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      await Geolocator.openLocationSettings();
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    return await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+  }
+
+  Future<dynamic> getAddressFromLatLong(Position position) async {
+    List<Placemark> placemarks =
+        await placemarkFromCoordinates(position.latitude, position.longitude);
+    lon = position.latitude;
+    lat = position.longitude;
+    print(placemarks);
+
+    Placemark place = placemarks[0];
+    address =
+        '${place.street}, ${place.subLocality}, ${place.locality}, ${place.postalCode}, ${place.country}';
+    country = '${place.country}';
+    print('[USE Country] : ${country}');
+  }
+}
